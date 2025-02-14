53514d4b5c472dfe8c00185fc7a36dbb01eeef40
aarizaf <104509368+aarizaf@users.noreply.github.com>
2025-02-13 23:03:19 -0500

    doocker

--- mostrar_tiempos.py
+++
@@ -1,8 +0,0 @@
-# mostrar_tiempos.py
-import csv
-
-# Leer el archivo CSV
-with open('resultados/resultados.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        print(f"Lenguaje: {row['Lenguaje']}, Tiempo: {row['Tiempo(ms)']} ms")
