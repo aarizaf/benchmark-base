8ed6f95bead87b1d5c52220f99a8f4bd19b60b65
aarizaf <104509368+aarizaf@users.noreply.github.com>
2025-02-13 22:59:52 -0500

    tiempos

--- mostrar_tiempos.py
+++ mostrar_tiempos.py
@@ -2,7 +2,7 @@
 import csv
 
 # Leer el archivo CSV
-with open('resultados.csv', newline='') as csvfile:
+with open('resultados/resultados.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         print(f"Lenguaje: {row['Lenguaje']}, Tiempo: {row['Tiempo(ms)']} ms")
