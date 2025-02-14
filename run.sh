e087f12f8f09c1a3cf1d1d8386d7233ae51c25b7
aarizaf <104509368+aarizaf@users.noreply.github.com>
2025-02-14 10:42:42 -0500

    holaa

--- run.sh
+++ run.sh
@@ -1,34 +1,18 @@
 #!/bin/bash
 
-# Clonar el repositorio con las soluciones
-git clone https://github.com/JP1604/dockerlanguages.git
-cd benchmark-ordenamiento
+# Definir el archivo de resultados
+RESULTADOS="resultados.txt"
 
-# Archivo CSV para resultados
-RESULTADOS="resultados/resultados.csv"
-echo "Lenguaje,Tiempo(ms)" > "$RESULTADOS"
-
-# Recorrer carpetas de lenguajes
-echo "Ejecutando benchmarks..."
-for dir in Lenguajes/*/; do
-  # Verifica si es un directorio válido
-  if [ -d "$dir" ]; then
-    LENGUAJE=$(basename "$dir")
-    echo "🔹 Ejecutando $LENGUAJE..."
-    
-    # Construir imagen Docker
-    docker build -t "${LENGUAJE//+/}-benchmark" "$dir"
-    
-    # Ejecutar contenedor y capturar tiempo
-    TIEMPO=$(docker run --rm "${LENGUAJE//+/}-benchmark")
-    
-    if [ -n "$TIEMPO" ]; then
-      echo "$LENGUAJE,$TIEMPO" >> "$RESULTADOS"
-      echo "✅ $LENGUAJE: $TIEMPO ms"
-    else
-      echo "❌ Error: Salida inesperada del contenedor para $LENGUAJE"
-    fi
+# Ejecutar contenedor y capturar tiempo
+for LENGUAJE in cpp java javascript python rust; do
+  TIEMPO=$(docker run --rm "${LENGUAJE//+/}-benchmark")
+  
+  if [ -n "$TIEMPO" ]; then
+    echo "$LENGUAJE,$TIEMPO" >> "$RESULTADOS"
+    echo "✅ $LENGUAJE: $TIEMPO ms"
+  else
+    echo "❌ Error: Salida inesperada del contenedor para $LENGUAJE"
   fi
 done
 
-echo "📊 Resultados guardados en $RESULTADOS"
+echo "📊 Resultados guardados en $RESULTADOS"
